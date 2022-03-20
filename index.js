541c60afcf5a36462c9ee25463e62e2f4826a79e
Vlad <vsakayafignavlad@gmail.com>
2022-03-21 02:17:05 +0300

    better click

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var flag = false;
-
+var lastOpen = 'none';
+// var mydata = JSON.parse(window.data);
+// alert(mydata[0].name);
+// alert(mydata[0].age);
+// alert(mydata[1].name);
+// alert(mydata[1].age);
 DivObject = function(name) {
     if (flag == false){
         this.div = document.createElement("div");
@@ -7,9 +12,24 @@ DivObject = function(name) {
         this.div.className = "panel" ;
         this.div.innerHTML = name;
         flag = true;
+        lastOpen = name;
     }else{
-        var div = document.getElementsByClassName("panel")[0];
-        div.parentNode.removeChild(div);
-        flag = false;
+        if (lastOpen == name){
+            var div = document.getElementsByClassName("panel")[0];
+            div.parentNode.removeChild(div);
+            flag = false;
+            lastOpen = 'none';
+        }else{
+            var div = document.getElementsByClassName("panel")[0];
+            div.parentNode.removeChild(div);
+            flag = false;
+            lastOpen = 'none';
+            this.div = document.createElement("div");
+            document.body.appendChild( this.div ) ;
+            this.div.className = "panel" ;
+            this.div.innerHTML = name;
+            flag = true;
+            lastOpen = name;
+        }
     }
 }
