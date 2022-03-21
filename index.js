75e9c73fe97edac568162aea292629f7fe469024
Vladislav Ainshtein <ainshtein.v@liceum1535.ru>
2022-03-21 11:35:42 +0300

    link

--- index.js
+++ index.js
@@ -4,33 +4,33 @@ var lastOpen = 'none';
 var data = {
     "1": {
         "name": "Остров 1",
-        "Description": "Описание обычного красивого острова<br/>Этот остров красивый",
+        "description": "Описание обычного красивого острова<br/>Этот остров красивый",
         "price": "5",
-        "link": "t.me/vladislav_ain_bot?start=1"
+        "link": "<a href='https://t.me/vladislav_ain_bot?start=1'>Купить в боте</a>"
     },
     "2": {
         "name": "Остров 2",
         "description": "Описание обычного красивого острова<br/>Этот остров красивый",
         "price": "3",
-        "link": "t.me/vladislav_ain_bot?start=1"
+        "link": "<a href='https://t.me/vladislav_ain_bot?start=1'>Купить в боте</a>"
     },
     "3": {
         "name": "Остров 3",
         "description": "Описание обычного красивого острова<br/>Этот остров красивый",
         "price": "1",
-        "link": "t.me/vladislav_ain_bot?start=1"
+        "link": "<a href='https://t.me/vladislav_ain_bot?start=1'>Купить в боте</a>"
     },
     "4": {
         "name": "Остров 4",
         "description": "Описание обычного красивого острова<br/>Этот остров красивый",
         "price": "5",
-        "link": "t.me/vladislav_ain_bot?start=1"
+        "link": "<a href='https://t.me/vladislav_ain_bot?start=1'>Купить в боте</a>"
     },
     "5": {
         "name": "Остров 5",
         "description": "Описание обычного красивого острова<br/>Этот остров красивый",
         "price": "76",
-        "link": "t.me/vladislav_ain_bot?start=1"
+        "link": "<a href='https://t.me/vladislav_ain_bot?start=1'>Купить в боте</a>"
     }
 }
 
